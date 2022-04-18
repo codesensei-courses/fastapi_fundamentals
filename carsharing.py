458615a003ab8b8806029b68fa65680b6199eefb
reindert <info@codesensei.nl>
2022-04-18 21:05:22 +0200

    After module 4: Serving Data With FastAPI

--- carsharing.py
+++ carsharing.py
@@ -1,17 +1,41 @@
-from datetime import datetime
+from typing import List, Optional
 
-from fastapi import FastAPI
+import uvicorn
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
+db = [
+    {"id": 1, "size": "s", "fuel": "gasoline", "doors": 3, "transmission": "auto"},
+    {"id": 2, "size": "s", "fuel": "electric", "doors": 3, "transmission": "auto"},
+    {"id": 3, "size": "s", "fuel": "gasoline", "doors": 5, "transmission": "manual"},
+    {"id": 4, "size": "m", "fuel": "electric", "doors": 3, "transmission": "auto"},
+    {"id": 5, "size": "m", "fuel": "hybrid", "doors": 5, "transmission": "auto"},
+    {"id": 6, "size": "m", "fuel": "gasoline", "doors": 5, "transmission": "manual"},
+    {"id": 7, "size": "l", "fuel": "diesel", "doors": 5, "transmission": "manual"},
+    {"id": 8, "size": "l", "fuel": "electric", "doors": 5, "transmission": "auto"},
+    {"id": 9, "size": "l", "fuel": "hybrid", "doors": 5, "transmission": "auto"}
+]
 
 
-@app.get("/")
-def welcome():
-    """Return a friendly welcome message."""
-    return {'message': "Welcome to the Car Sharing service!"}
+@app.get("/api/cars")
+def get_cars(size: str|None = None, doors: int|None = None) -> list:
+# def get_cars(size: Optional[str] = None, doors: Optional[str] = None) -> List:
+    result = db
+    if size:
+        result = [car for car in result if car['size'] == size]
+    if doors:
+        result = [car for car in result if car['doors'] >= doors]
+    return result
 
 
-@app.get("/date")
-def date():
-    """Return the current date/time."""
-    return {'date': datetime.now()}
+@app.get("/api/cars/{id}")
+def car_by_id(id: int) -> dict:
+    result = [car for car in db if car['id'] == id]
+    if result:
+        return result[0]
+    else:
+        raise HTTPException(status_code=404, detail=f"No car with id={id}.")
+
+
+if __name__ == "__main__":
+    uvicorn.run("carsharing:app", reload=True)
